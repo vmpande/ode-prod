f1a185d490d96e9ebe80f8a3674d68f1321578b3
Vinayak Pande <Vinayak Pande>
2016-08-26 16:59:32 -0400

    color

--- js-custom/MidEastMap.php
+++ js-custom/MidEastMap.php
@@ -89,8 +89,8 @@
 			                  '#bcc64d';*/
 			return d > 100 ? '#c55441': //'#ca6554' :
 				   d > 10 ? '#d6877a' :
-				   d = 0 ? '#8f9394' :
-			                  '#e7bab3';
+				   d > 0 ?  '#e7bab3':
+			                '#8f9394';
 		}
 
 		function style(feature) 
